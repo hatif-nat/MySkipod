30fded34a15f12e096f7972d6ca7e4c05d2fb0c1
hatif-nat <64038968+hatif-nat@users.noreply.github.com>
2020-12-19 00:14:08 +0300

    some changes 22

--- mpi/src/test_mpi.cpp
+++ mpi/src/test_mpi.cpp
@@ -1,130 +1,220 @@
-#include <iostream>
-#include <random>
-#include <iomanip>
+#include <stdio.h>
+#include <stdlib.h>
 #include <mpi.h>
-#include <malloc.h>
 #include <fstream>
+#include <iomanip>
 
-using namespace std;
+int main(int argc, char *argv[])
+{
+    int i,j,k, kk;
+    int n, error, myrank, numproc, source, request;
+    int start_row, last_row, nrow;
+    float **A, *leading_row;
+    double start_time, end_time, start_algo;
+    double determinant = 1;
 
-enum errorCode {
-    ZERO_DET
-};
+    MPI_Init(&argc, &argv); // Инициализируем коммуникатор MPI - через него будет происходить взаимодействие группы процессов
 
-double **alloc_2d(int rows, int cols) {
-    double *data = (double *)malloc(rows*cols*sizeof(double));
-    double **array= (double **)malloc(rows*sizeof(double*));
-    for (int i=0; i<rows; i++)
-        array[i] = &(data[cols*i]);
+    MPI_Comm_size(MPI_COMM_WORLD, &numproc); //число процессов в коммуникаторе
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank); //номер процесса в коммуникаторе (нумерация с 0)
 
-    return array;
-}
+    if (!myrank) {
+        n = (argc>1) ? atoi(argv[1]) : 512;
+    }
 
-double** initMatrix(size_t n) {
-    double **m = alloc_2d(n, n);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            m[i][j] = (double)(rand() % 100);
+    // Приостановка процессов до выхода ВСЕХ процессов коммуникатора в заданную точку синхронизации
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD); // Широковещательная рассылка n всем процессам
+
+    if (!myrank) start_time = MPI_Wtime();
+
+    nrow = n/numproc;
+    start_row = nrow * myrank;
+    last_row = start_row + nrow - 1;
+    if (myrank==numproc-1) {
+        last_row = n-1;
+        nrow = last_row - start_row + 1;
+    }
+
+    // Выделяем память под элементы рабочей(расширенной) матрицы (исходная + единичная, приписанная справа) - размерность: n * (n*2)
+    // Каждый процесс выделает себе nrow строчек и хранит только их
+    A = (float**) malloc(nrow * (n) * sizeof(float));
+
+    if (A==NULL){
+        MPI_Abort(MPI_COMM_WORLD, MPI_ERR_OTHER);
+        return -1;
+    }
+    for (i=0; i<nrow; i++)
+    {
+        A[i] = (float*) malloc((n) * sizeof(float));
+        if (A[i]==NULL){
+            MPI_Abort(MPI_COMM_WORLD, MPI_ERR_OTHER);
+            return -1;
+        }
+    }
+
+    leading_row = (float*) malloc((n) * sizeof(float));
+
+    if (leading_row==NULL){
+        MPI_Abort(MPI_COMM_WORLD, MPI_ERR_OTHER);
+        return -1;
+    }
+
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // Заполняем исходную матрицу
+    if (!myrank)
+    {
+        for (i=0; i<n; i++)
+        {
+            k = i / (n/numproc);
+            if (k>=numproc) k = numproc-1;
+            if (!k) {
+                for (j = 0; j < n; j++) {
+                    A[i][j] = (rand()) % 10;
+                }
+            }
+            else
+            {
+                for (j=0; j<n; j++) {
+                    leading_row[j] = (rand()) % 10
+                }
+                MPI_Send(leading_row, n, MPI_FLOAT, k, 1, MPI_COMM_WORLD);
             }
         }
     }
-    return m;
-}
 
-void printMatrix(double **m, size_t n) {
-    cout << "{\n";
-    for (int i = 0; i < n; i++) {
-        cout << "    {";
-        for (int j = 0; j < n; j++) {
-            cout << ((m[i][j] >= 0.0 && m[i][j] < 10.0) ? " " : "") << fixed << setprecision(3) << m[i][j] << (j != n - 1 ? ", " : "");
-        }
-        cout << "}" << (i != n - 1 ? "," : "") << endl;
-    }
-    cout << '}';
-}
-
-int main(int argc, char **argv) {
-
-    int id, p;
-    double a, c;
-
-    /*
-        id - номер текущего процесса
-        p - количесво процессов
-        c, a - коэф-ты, которые нужно будет умножать элементы
-    */
-    double time_start, time_end;
-    double **m;
-
-    bool test = true;
-    int matrix_size = 3;
-    if (argc > 1) {
-        if ((matrix_size = stoi(argv[1])) > 0) {
-            test = false;
+    // Каждый процесс получает свои nrow строчек (считанные процессом 0)
+    if (myrank) {
+        for (i = 0; i < nrow; i++) {
+            MPI_Recv(A[i], n, MPI_FLOAT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
     }
 
-    double cur_row[matrix_size];
-
-    MPI_Status status;
-
-    MPI_Init(&argc, &argv);
-    // id данного процесса
-    MPI_Comm_rank(MPI_COMM_WORLD, &id);
-    // количество процессов
-    MPI_Comm_size(MPI_COMM_WORLD, &p);
-
-    m = initMatrix(matrix_size, ZERO);
-
-    // если это главный процесс, то он инициализирует матрицу
-    if (id == 0) {
-        cout << "\nWorkers: " << p << endl;
-        m = initMatrix(matrix_size, RANDOM);
-
-        time_start = MPI_Wtime();
-    }
-
-    // все процессы получают исходную матрицу и единичную матрицу
     MPI_Barrier(MPI_COMM_WORLD);
-    MPI_Bcast(&(m[0][0]), matrix_size*matrix_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    if (!myrank) start_algo= MPI_Wtime();
 
-    for (int i = 0; i < matrix_size; i++) {
-        MPI_Barrier(MPI_COMM_WORLD);
-        if (id) {
-            a = -1.0 / m[i][i];
-            for (int j = id - 1; j < matrix_size; j += p - 1) {
-                c = m[j][i] * a;
-                if (j != i) {
-                    for (int k = 0; k < matrix_size; k++) {
-                        m[j][k] += c * m[i][k];
+    // ПРЯМОЙ ХОД ГАУССА
+    for (i=0; i<n; i++)
+    {
+        source = i / (n/numproc); //процесс-источник (содержащий лидирующую строку)
+        if (source >= numproc) source = numproc-1;
+
+        // Выбираем строку с ненулевым ведущим элементом
+        if (myrank == source)
+        {
+            k = 0;
+            if (A[i-start_row][i]==0)
+            {
+                for (k=i-start_row+1; k<nrow; k++)
+                {
+                    if (A[k][i] != 0)
+                    {
+                        // меняем строки местами
+                        for (j=0; j<n; j++)
+                        {
+                            float t=A[k][j];
+                            A[k][j]=A[i-start_row][j];
+                            A[i-start_row][j]=t;
+                        }
+                        break;
                     }
                 }
+            }
+            if (k == nrow)
+            {
+                for (j=myrank+1; j<numproc && k; j++)
+                {
+                    MPI_Send(&j, 1, MPI_INT, j, 3, MPI_COMM_WORLD); //отправляем процессу "сигнал" о замене строчек
+                    MPI_Recv(leading_row, n, MPI_FLOAT, j, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                    if (leading_row[i] != 0)
+                    {
+                        MPI_Send(A[i-start_row], n, MPI_FLOAT, j, 2, MPI_COMM_WORLD);
+                        for (kk=0; kk<n; kk++) A[i-start_row][kk] = leading_row[kk]; //заменяем строчку
+                        k=0;
+                    }
+                    else MPI_Send(leading_row, n, MPI_FLOAT, j, 2, MPI_COMM_WORLD);
+                }
+                kk = 0;
+                for (; j<numproc; j++)
+                    MPI_Isend(&kk, 1, MPI_INT, j, 3, MPI_COMM_WORLD, &request);
+            }
+            else
+            {
+                kk = 0; // Не требуется строчка ни от какого из процессов
+                for (j=myrank+1; j<numproc; j++)
+                    MPI_Isend(&kk, 1, MPI_INT, j, 3, MPI_COMM_WORLD, &request);
+            }
+            if (k == nrow)
+            {
+                MPI_Abort(MPI_COMM_WORLD, MPI_ERR_OTHER);
+                return -1;
+            }
+        }
 
-                MPI_Send(m[j], matrix_size, MPI_DOUBLE, 0, 123, MPI_COMM_WORLD);
+        else if (myrank > source)
+        {
+            MPI_Recv(&kk, 1, MPI_INT, source, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            if (kk) //нужно отправить строчку
+            {
+                for (j=0; j<nrow-1 && A[j][i]==0; j++) ; //находим строчку с ненулевым ведущим элементом в каждом процессе
+                MPI_Send(A[j], n, MPI_FLOAT, source, 2, MPI_COMM_WORLD); //отправялем ее (или последнюю, если не было найдено)
+                MPI_Recv(A[j], n, MPI_FLOAT, source, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE); //получаем обратно (возможно измененная)
             }
-        } else {
-            for (int j = 0; j < matrix_size; j++) {
-                MPI_Recv(m[j], matrix_size, MPI_DOUBLE, j % (p - 1) + 1, 123, MPI_COMM_WORLD, &status);
+        }
+
+        // Делим всю строку на ее ведущий элемент(делаем ведущий элемент = единице)
+        if (i >= start_row && i <= last_row) //(myrank == source)
+        {
+            // !!!@@#$@!MPI_Send(A[i-start_row][i], 1, MPI_DOUBLE, source, 2, MPI_COMM_WORLD);
+            for (j=i+1; j<n; j++) A[i-start_row][j] /= A[i-start_row][i];
+            A[i-start_row][i] = 1;
+            for (j=0; j<n; j++) leading_row[j] = A[i-start_row][j];
+
+            // Отправка лидирующей строки(содержащей ведущий элемент) 'нижележащим' процессам
+            for (k=myrank + 1; k<numproc; k++)
+                MPI_Send(leading_row, n, MPI_FLOAT, k, 1, MPI_COMM_WORLD);
+        }
+
+        // С помощью ведущего элемента аннулируем все расположенные под ним элементы i-го столбца
+        if (i < last_row)
+        {
+            if (myrank != source){ //сам себе не отправлял
+                MPI_Recv(leading_row, n, MPI_FLOAT, source, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                kk = 0;
             }
-            double u = m[i][i];
-            for (int k = 0; k < matrix_size; k++) {
-                m[i][k] /= u;
+            else kk=i-start_row+1;
+
+            for (k=kk; k<nrow; k++)
+            {
+                for (j=i+1; j<n; j++)
+                    A[k][j] -= leading_row[j]*A[k][i];
+                A[k][i] = 0;
             }
         }
         MPI_Barrier(MPI_COMM_WORLD);
-        MPI_Bcast(&(m[0][0]), matrix_size*matrix_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     }
+
 
     MPI_Barrier(MPI_COMM_WORLD);
 
-    if (id == 0) {
-        time_end = MPI_Wtime();
-        cout << "\nMPI execution time: " << time_end - time_start << endl;
+    if (!myrank) {
+        end_time = MPI_Wtime();
         std::ofstream fout(argv[2], ios::app);
         fout.is_open();
-        fout << p << "," << argv[1] << "," << time_end - time_start << '\n';
+        fout << numproc << "," << argv[1] << "," << end_time - start_algo << '\n';
         fout.close();
     }
+
+    // Освобождаем память, выделенную под матрицу
+    for (i=0; i<nrow; i++) free(A[i]);
+    free(A);
+    free(leading_row);
+
     MPI_Finalize();
+
     return 0;
 }
+
