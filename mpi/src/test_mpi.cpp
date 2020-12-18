b9326282b3eaea589e928b49231213effcedc4c9
hatif-nat <64038968+hatif-nat@users.noreply.github.com>
2020-12-18 23:00:21 +0300

    some changes 21

--- mpi/src/test_mpi.cpp
+++ mpi/src/test_mpi.cpp
@@ -7,12 +7,6 @@
 
 using namespace std;
 
-enum MatrixType {
-    IDENTITY,
-    RANDOM,
-    ZERO
-};
-
 enum errorCode {
     ZERO_DET
 };
@@ -26,7 +20,7 @@ double **alloc_2d(int rows, int cols) {
     return array;
 }
 
-double** initMatrix(size_t n, MatrixType type = RANDOM) {
+double** initMatrix(size_t n) {
     double **m = alloc_2d(n, n);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -60,7 +54,7 @@ int main(int argc, char **argv) {
         c, a - коэф-ты, которые нужно будет умножать элементы
     */
     double time_start, time_end;
-    double **m, **I;
+    double **m;
 
     bool test = true;
     int matrix_size = 3;
@@ -81,24 +75,11 @@ int main(int argc, char **argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &p);
 
     m = initMatrix(matrix_size, ZERO);
-    I = initMatrix(matrix_size, IDENTITY);
 
     // если это главный процесс, то он инициализирует матрицу
     if (id == 0) {
         cout << "\nWorkers: " << p << endl;
-
-        if (test) {
-            m[0][0] = 1, m[0][1] = 2, m[0][2] = 1,
-            m[1][0] = 3, m[1][1] = 5, m[1][2] = 2,
-            m[2][0] = 2, m[2][1] = 3, m[2][2] = 3;
-        } else {
-            m = initMatrix(matrix_size, RANDOM);
-        }
-
-        if (matrix_size <= 10) {
-            cout << "\nInput matrix (generated randomly):\n\n";
-            printMatrix(m, matrix_size);
-        }
+        m = initMatrix(matrix_size, RANDOM);
 
         time_start = MPI_Wtime();
     }
@@ -115,41 +96,30 @@ int main(int argc, char **argv) {
                 c = m[j][i] * a;
                 if (j != i) {
                     for (int k = 0; k < matrix_size; k++) {
-                        I[j][k] += c * I[i][k];
                         m[j][k] += c * m[i][k];
                     }
                 }
 
                 MPI_Send(m[j], matrix_size, MPI_DOUBLE, 0, 123, MPI_COMM_WORLD);
-                MPI_Send(I[j], matrix_size, MPI_DOUBLE, 0, 123, MPI_COMM_WORLD);
             }
         } else {
             for (int j = 0; j < matrix_size; j++) {
                 MPI_Recv(m[j], matrix_size, MPI_DOUBLE, j % (p - 1) + 1, 123, MPI_COMM_WORLD, &status);
-                MPI_Recv(I[j], matrix_size, MPI_DOUBLE, j % (p - 1) + 1, 123, MPI_COMM_WORLD, &status);
             }
             double u = m[i][i];
             for (int k = 0; k < matrix_size; k++) {
-                I[i][k] /= u;
                 m[i][k] /= u;
             }
         }
         MPI_Barrier(MPI_COMM_WORLD);
         MPI_Bcast(&(m[0][0]), matrix_size*matrix_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-        MPI_Bcast(&(I[0][0]), matrix_size*matrix_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     }
 
     MPI_Barrier(MPI_COMM_WORLD);
 
     if (id == 0) {
-        if (matrix_size <= 10 ) {
-            cout << "\n\nResult:\n\n";
-            printMatrix(I, matrix_size);
-        }
-        if (id == 0) {
-            time_end = MPI_Wtime();
-            cout << "\nMPI execution time: " << time_end - time_start << endl;
-        }
+        time_end = MPI_Wtime();
+        cout << "\nMPI execution time: " << time_end - time_start << endl;
         std::ofstream fout(argv[2], ios::app);
         fout.is_open();
         fout << p << "," << argv[1] << "," << time_end - time_start << '\n';
