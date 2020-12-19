f2589703ea6f50d760e75d68256a8db7059f544e
hatif-nat <64038968+hatif-nat@users.noreply.github.com>
2020-12-19 05:18:09 +0300

    full changes

--- mpi/src/test_mpi.cpp
+++ mpi/src/test_mpi.cpp
@@ -8,46 +8,34 @@
 
 using namespace std;
 
-double **allocateMatrix(int rows, int cols) {
-    double *data = (double *)malloc(rows*cols*sizeof(double));
-    double **array= (double **)malloc(rows*sizeof(double*));
-    for (int i=0; i<rows; i++)
-        array[i] = &(data[cols*i]);
+double **allocateMatrix(int n) {
+    double *line = (double *)malloc(n*n*sizeof(double));
+    double **matrix= (double **)malloc(n*sizeof(double*));
+    for (int i=0; i<n; i++)
+        matrix[i] = &(line[n*i]);
 
-    return array;
+    return matrix;
 }
 
-double** initMatrix(size_t n) {
-    double **m = allocateMatrix(n, n);
+double** initMatrix(int n) {
+    double **matrix = allocateMatrix(n);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            m[i][j] = (double)(rand() % 100);
+            matrix[i][j] = (double)(rand() % 10);
         }
     }
-    return m;
+    return matrix;
 }
 
 int main(int argc, char **argv) {
 
-    int id, p;
-    double a, c;
+    int id, allProc;
 
-    /*
-        id - номер текущего процесса
-        p - количесво процессов
-        c, a - коэф-ты, которые нужно будет умножать элементы
-    */
     double time_start, time_end;
-    double **m;
-    int request = 0;
+    double **matrix;
+    double determinant = 1;
+    int size = atoi(argv[1]);
 
-    bool test = true;
-    int matrix_size = 3;
-    if (argc > 1) {
-        if ((matrix_size = atoi(argv[1])) > 0) {
-            test = false;
-        }
-    }
 
     MPI_Status status;
 
@@ -55,48 +43,55 @@ int main(int argc, char **argv) {
     // id данного процесса
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
     // количество процессов
-    MPI_Comm_size(MPI_COMM_WORLD, &p);
+    MPI_Comm_size(MPI_COMM_WORLD, &allProc);
 
-    m = initMatrix(matrix_size);
+    matrix = initMatrix(size);
 
     // если это главный процесс, то он инициализирует матрицу
     if (id == 0) {
-        cout << matrix_size << ", " << p << ", ";
+        cout << size << ", " << allProc << ", ";
         time_start = MPI_Wtime();
     }
 
     // все процессы получают исходную матрицу и единичную матрицу
     MPI_Barrier(MPI_COMM_WORLD);
-    MPI_Bcast(&(m[0][0]), matrix_size*matrix_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&(matrix[0][0]), size*size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    double lead[matrix_size];
-    int block = matrix_size / (p - 1),
+    double lead[size];
+    int block = size / (allProc - 1),
             start_row = block * id,
             end_row = block * (id + 1);
-    if (id == p - 1)
-        end_row = matrix_size;
+    if (id == allProc - 1)
+        end_row = size;
 
-    for (int i = 0; i < matrix_size; i++) {
+    for (int i = 0; i < size; i++) {
 
         if (id == i / block) {
-            for (int j = 0; j < matrix_size ; j++) {
-                lead[j] = m[i][j];
+            for (int j = 0; j < size ; j++) {
+                lead[j] = matrix[i][j];
             }
         }
 
         MPI_Barrier(MPI_COMM_WORLD);
-        MPI_Bcast(lead, matrix_size , MPI_DOUBLE, i / block, MPI_COMM_WORLD);
+        MPI_Bcast(lead, size , MPI_DOUBLE, i / block, MPI_COMM_WORLD);
 
         for (int j = start_row; j < end_row; j++) {
             if (j == i) {
-                double d = m[i][i];
-                for (int k = 0; k < matrix_size ; k++)
-                    m[j][k] /= d;
+                double d = matrix[i][i];
+                if (i < end_row && i >= start_row) {
+                    MPI_Send(d, 1, MPI_Double, id, 2, MPI_COMM_WORLD); //отправялем часть определителя
+                }
+                if (!id) {
+                    MPI_Recv(d, n * 2, MPI_FLOAT, source, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                    determinant *= d;
+                }
+                for (int k = 0; k < size ; k++)
+                    matrix[j][k] /= d;
                 continue;
             }
-            double d = m[j][i] / lead[i];
-            for (int k = 0; k < matrix_size ; k++) {
-                m[j][k] -= d * lead[k];
+            double d = matrix[j][i] / lead[i];
+            for (int k = 0; k < size ; k++) {
+                matrix[j][k] -= d * lead[k];
             }
         }
 
@@ -108,10 +103,9 @@ int main(int argc, char **argv) {
     if (id == 0) {
 
         time_end = MPI_Wtime();
-        cout << time_end - time_start << endl;
         std::ofstream fout(argv[2], ios::app);
         fout.is_open();
-        fout << p << "," << argv[1] << "," << time_end - time_start << '\n';
+        fout << allProc << "," << argv[1] << "," << time_end - time_start << '\n';
         fout.close();
     }
     MPI_Finalize();
